c838011f1dbdfd1761fbdf8b5177a99314ba456c
AshaJG <54907036+AshaJG@users.noreply.github.com>
2021-12-07 02:50:38 -0500

    User can edit their profile name, their profile picture , they can change their password

--- Note_Taker/EditProfileViewController.swift
+++ Note_Taker/EditProfileViewController.swift
@@ -6,24 +6,89 @@
 //
 
 import UIKit
+import Parse
 
-class EditProfileViewController: UIViewController {
+class EditProfileViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var profilePictureImageView: UIImageView!
+    
+    @IBOutlet weak var displayUsernameTextfield: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var passwordRepeatTextfield: UITextField!
+    
+    //connects to the profile to display changes
+       var opener: ProfileViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Edit Profile"
-        // Do any additional setup after loading the view.
+      // Do any additional setup after loading the view.
+      //load user details
+        let userName = PFUser.current()?.object(forKey: "username")
+        displayUsernameTextfield.text = userName as! String
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func chooseProfileButtonTapped(_ sender: Any) {
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self;
+        myPickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
+        self.present(myPickerController, animated: true, completion : nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        let myUser:PFUser = PFUser.current()!
+        //make certain that display name is never empty
+        if((displayUsernameTextfield.text?.isEmpty) == nil){
+            let myAlert = UIAlertController(title: "Alert", message: "display name can't be left empty", preferredStyle: UIAlertController.Style.alert)
+            let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil)
+            myAlert.addAction(okAction)
+            self.present(myAlert, animated:true, completion:nil)
+            return
+         }
+        //if the current user changes their password
+        if((passwordTextField.text != nil) && (passwordTextField.text != passwordRepeatTextfield.text)){
+                var myAlert = UIAlertController(title: "Alert", message: "passwords do not match", preferredStyle: UIAlertController.Style.alert)
+                let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil)
+                myAlert.addAction(okAction)
+                self.present(myAlert, animated:true, completion:nil)
+                return
+            }
+       
+        //set new values
+        let userName = displayUsernameTextfield.text
+        myUser.setObject(userName, forKey: "username")
+               
+        //set new password
+        if(passwordTextField.text != nil){
+            let userpassword = passwordTextField.text
+            myUser.password = userpassword
+        }
+               
+        //set new profile picture
+              /* if(profilePictureImageView != nil){
+                   let profileFileObject = PFFileObject(data:profilePictureImageView)
+                   myUser.setObject(profileFileObject, forKey: "profile_picture")
+               }*/
+               
+        myUser.saveInBackground{(success, error) in
+        if success {
+            self.dismiss(animated: true, completion: {() -> Void in self.opener.loadUserDetails()})
+            } else
+            {
+                   print("error")
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+           profilePictureImageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+           self.dismiss(animated: true, completion : nil)
+           
+       }
 }
--- Note_Taker/ProfileViewController.swift
+++ Note_Taker/ProfileViewController.swift
@@ -2,21 +2,52 @@
 //  ProfileViewController.swift
 //  Note_Taker
 //
-//  Created by Ashley Jo-ann Grant on 12/3/21.
+//  Created by Ashley Jo-ann Grant on 12/7/21.
 //
 
 import UIKit
+import Parse
 
-
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBAction func Return(_ sender: Any) {
-       
-    }
+    @IBOutlet weak var userFullNameLabel: UILabel!
+    
+    @IBOutlet weak var profilePhotoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //title = "Profile"
-    }
 
+        // Do any additional setup after loading the view.
+        loadUserDetails()
+    }
+    
+    @IBAction func selectProfileButton(_ sender: Any) {
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self;
+        myPickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
+        self.present(myPickerController, animated: true, completion : nil)
+    }
+    
+    @IBAction func editButtonTapped(_ sender: Any) {
+        var editProfile = self.storyboard?.instantiateViewController(withIdentifier: "EditProfileViewController") as! EditProfileViewController
+        editProfile.opener = self
+                
+        let editProfileNav = UINavigationController(rootViewController: editProfile)
+        self.present(editProfileNav, animated: true , completion : nil)
+        
+    }
+    
+    func loadUserDetails(){
+        let userName = PFUser.current()?.object(forKey: "username") as! String
+        userFullNameLabel.text = userName
+    }
+    
+ 
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+            profilePhotoImageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        self.dismiss(animated: true, completion : nil)
+            
+        }
+    
 }
